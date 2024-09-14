1f3cab9126fb55ef7c46579fc97b7f3d0f2e91bf
Ting En Li <ss7826603ss@gmail.com>
2024-09-14 22:23:36 +0800

    Update pouchy.py

--- pouchy.py
+++ pouchy.py
@@ -1,10 +1,3 @@
-def download_OPass():
-    """
-    Download the OPass App from Google Play or the App Store.
-    """
-    return True
-
-
 def visit(location):
     """
     Go to the designated location and complete the task.
